b7386b7589698adfba26b87ae5ee504dfbed2781
ashrafyawar <mayawar@gtu.edu.tr>
2020-07-21 13:16:06 +0300

    some changes

--- Mobile App/BallTracker/app/src/main/java/com/example/balltracker/MainActivity.java
+++ Mobile App/BallTracker/app/src/main/java/com/example/balltracker/MainActivity.java
@@ -18,11 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //below code has to jump to login activity but it jumps by skipping loading page !!! should be fixed to test below code first comment progressBarLoading(); function.
          Intent intentLoginPage = new Intent(MainActivity.this,LoadingActivity.class);
          startActivity(intentLoginPage);
-
-        //to test below code comment above intent if already not commented
     }
 }
